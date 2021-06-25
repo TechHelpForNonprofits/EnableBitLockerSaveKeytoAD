30145a27d2fb328fd38a9a92d45e4c7fc2548bfe
TechHelpForNonProfits <78708065+TechHelpForNonprofits@users.noreply.github.com>
2021-06-25 16:01:46 -0500

    Update Bitlock-VM.ps1

--- Bitlock-VM.ps1
+++ Bitlock-VM.ps1
@@ -1,9 +1,12 @@
-﻿$vol = (get-wmiobject -Class Win32_Volume | where{$_.drivetype -eq '5'}  )
+#This works for Hyper-V virtual machines only
+#finds the mounted ISO and ejects it since Bitlocker won't work if it's still mounted
+$vol = (get-wmiobject -Class Win32_Volume | where{$_.drivetype -eq '5'} )
 $Eject =  New-Object -comObject Shell.Application
 $Eject.NameSpace(17).ParseName($vol.driveletter).InvokeVerb(“Eject”)
 
 Start-Sleep -s 8
 
+#checks for key and if one doesn't exist it generates one
 $keyID = Get-BitLockerVolume -MountPoint c: | select -ExpandProperty keyprotector | 
             where {$_.KeyProtectorType -eq 'RecoveryPassword'} #captures key
 
@@ -13,6 +16,7 @@ If ($keyID -eq $Null) {
             where {$_.KeyProtectorType -eq 'RecoveryPassword'} #captures key
 }
 
+#enables Bitlocker and saves key to AD
 Backup-BitLockerKeyProtector -MountPoint c: -KeyProtectorId $keyID.KeyProtectorId
 Enable-BitLocker -MountPoint C: -SkipHardwareTest -RecoveryPasswordProtector
 
