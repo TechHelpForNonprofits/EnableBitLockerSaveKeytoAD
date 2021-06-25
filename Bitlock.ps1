50a2431588d784f6fb6360a3f280b4c3e46ce1ab
TechHelpForNonProfits <78708065+TechHelpForNonprofits@users.noreply.github.com>
2021-06-25 16:03:45 -0500

    Update Bitlock.ps1

--- Bitlock.ps1
+++ Bitlock.ps1
@@ -1,9 +1,12 @@
-﻿$vol = (get-wmiobject -Class Win32_Volume | where{$_.drivetype -eq '2'}  )
+#Used to enable Bitlocker on physical machines only. Does not work on VMs
+#Dismounts external device
+$vol = (get-wmiobject -Class Win32_Volume | where{$_.drivetype -eq '2'}  )
 $Eject =  New-Object -comObject Shell.Application
 $Eject.NameSpace(17).ParseName($vol.driveletter).InvokeVerb(“Eject”)
 
 Start-Sleep -s 8
 
+#generates key if one does not exist
 $keyID = Get-BitLockerVolume -MountPoint c: | select -ExpandProperty keyprotector | 
             where {$_.KeyProtectorType -eq 'RecoveryPassword'} #captures key
 
@@ -13,6 +16,7 @@ If ($keyID -eq $Null) {
             where {$_.KeyProtectorType -eq 'RecoveryPassword'} #captures key
 }
 
+#enables Bitlocker and saves key to AD
 Backup-BitLockerKeyProtector -MountPoint c: -KeyProtectorId $keyID.KeyProtectorId
 Enable-BitLocker -MountPoint C: -SkipHardwareTest -RecoveryPasswordProtector
 
