b9ca8c208b8a08db26f36a019903812c5d696878
TechHelpForNonProfits <78708065+TechHelpForNonprofits@users.noreply.github.com>
2021-06-25 15:58:46 -0500

    Add files via upload

---
+++ Bitlock.ps1
@@ -0,0 +1,23 @@
+﻿$vol = (get-wmiobject -Class Win32_Volume | where{$_.drivetype -eq '2'}  )
+$Eject =  New-Object -comObject Shell.Application
+$Eject.NameSpace(17).ParseName($vol.driveletter).InvokeVerb(“Eject”)
+
+Start-Sleep -s 8
+
+$keyID = Get-BitLockerVolume -MountPoint c: | select -ExpandProperty keyprotector | 
+            where {$_.KeyProtectorType -eq 'RecoveryPassword'} #captures key
+
+If ($keyID -eq $Null) {
+    cmd /c manage-bde.exe -protectors -add c: -recoverypassword #generates a Numerical Password
+    $keyID = Get-BitLockerVolume -MountPoint c: | select -ExpandProperty keyprotector | 
+            where {$_.KeyProtectorType -eq 'RecoveryPassword'} #captures key
+}
+
+Backup-BitLockerKeyProtector -MountPoint c: -KeyProtectorId $keyID.KeyProtectorId
+Enable-BitLocker -MountPoint C: -SkipHardwareTest -RecoveryPasswordProtector
+
+
+
+
+
+
